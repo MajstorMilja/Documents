aa7281e337590bbfdc8fdcf1ce08268bff348bc4
Miljan Stanojevic <miljanbg2@gmail.com>
2023-12-07 19:27:41 +0100

    fix

--- Dokument-api/DokumentCodeFirst/obj/Debug/net6.0/DokumentCodeFirst.AssemblyInfo.cs
+++ Dokument-api/DokumentCodeFirst/obj/Debug/net6.0/DokumentCodeFirst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DokumentCodeFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f9cad6602b0cb32ffb97fe4d1983df5261286a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DokumentCodeFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DokumentCodeFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
